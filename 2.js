27e8481fa142ec413732b1ed0b273634ca91a3f1
MillyP (she/her) <palmermilly9@gmail.com>
2021-10-27 16:58:19 +0100

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,31 @@
+function transform (arg1) {
+for(let i=0; arg1.length; i++) {
+     arg1.sort(); 
+    return arg1;
+ }
+};
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+console.log(transform(["mess!,", "What", 'a']));
