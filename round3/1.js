53ddc88135bc2b414564126db6738403e9ef4c17
MillyP <millypalmer1@hotmail.co.uk>
2021-11-24 01:41:59 +0000

    New round

---
+++ round3/1.js
@@ -0,0 +1,34 @@
+function transform(arr){
+    return Math.max.apply(null, arr);
+ }
+ function transform2(arr){
+    return Math.min.apply(null, arr);
+ }
+ 
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ //
+ console.log(transform([2,89,13,35]));
+ console.log(transform2([4,8,54,23]));
