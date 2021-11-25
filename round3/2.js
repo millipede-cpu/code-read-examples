01d8c21938ab898c6ecde3c6d1cf9f4a5777d92e
MillyP <millypalmer1@hotmail.co.uk>
2021-11-25 16:17:34 +0000

    New function

---
+++ round3/2.js
@@ -0,0 +1,40 @@
+    function transform(arg1) {
+    let stuff = false;
+    arg1.forEach(el => {
+        if(el > 0) {
+        stuff = true;
+        }
+    });
+    return stuff;
+    }
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+    
+    console.log(transform([]));
+    console.log(transform([-6, -8, 0]));
+    console.log(transform([-3, 0, 102]));
+    console.log(transform([56]));
