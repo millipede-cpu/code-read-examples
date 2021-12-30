eda4339eb5c98ce41f8637165f09c12f61deb0ac
MillyP <millypalmer1@hotmail.co.uk>
2021-12-30 22:36:29 +0000

    New code-read-example

---
+++ round1/4.js
@@ -0,0 +1,36 @@
+    function transform(arg1) {
+    return arg1.map(n => n * n)
+    .filter((item) => item > 100)
+        }
+    
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+    
+    console.log(transform([2,75,666]));
+    console.log(transform([50, 75, 11]));
+    console.log(transform([3,6,9]));
