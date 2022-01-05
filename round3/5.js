fd8310216dad0b8c7a7606ed06f4171058b98336
MillyP <millypalmer1@hotmail.co.uk>
2022-01-05 16:26:24 +0000

    Flat map code read

---
+++ round3/5.js
@@ -0,0 +1,42 @@
+    function transform (arg1) {
+
+    return arg1.flatMap(n => [n, n + 1]);
+    
+    }
+    
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    console.log(transform([1, 3, 5]));
+    console.log(transform([5, 10, 15]));
+    console.log(transform(['number ']));
